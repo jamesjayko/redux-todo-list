fbcc03fa06b8243dfa9c6bb324e328660962685d
jay <jamesjayko@gmail.com>
2018-01-11 12:31:41 -0800

    redux set up complete

---
+++ src/reducers/todo_reducer.js
@@ -0,0 +1,14 @@
+import types from '../actions/types';
+
+const DEFAULT_STATEMENT = {
+    list: []
+}
+
+export default function(state= DEFAULT_STATEMENT, action){
+    switch (action.types) {
+  
+    
+        default:
+            return state;
+    }
+}
